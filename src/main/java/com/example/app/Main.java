37be06a07446e70a16b5aa2811c022d20de1a5c2
Karolina <karolina.migus98@icloud.com>
2023-06-13 17:19:14 +0200

    Read from file refactoring

--- src/main/java/com/example/app/Main.java
+++ src/main/java/com/example/app/Main.java
@@ -6,19 +6,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner scanner;
-    private static Boolean wasRead;
-
     public static void main(String[] args) {
 
         System.out.println("Traveling Salesman v1");
-        wasRead = false;
-        scanner = new Scanner(System.in);
+        boolean wasRead = false;
+        Scanner scanner = new Scanner(System.in);
 
         while (!wasRead) {
             System.out.print("Type in the filename: ");
             String filename = scanner.nextLine();
-            ReadFromFile reader = new ReadFromFile(filename);
             wasRead = ReadFromFile.read(filename);
         }
 
--- src/main/java/com/example/app/fileHandling/ReadFromFile.java
+++ src/main/java/com/example/app/fileHandling/ReadFromFile.java
@@ -1,25 +1,22 @@
 package com.example.app.fileHandling;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReadFromFile {
 
     private String filename;
-
-    public ReadFromFile(String filename) {
-        this.filename = filename;
-    }
-
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
 
     public static Boolean read(String filename) {
         File file = new File(filename);
